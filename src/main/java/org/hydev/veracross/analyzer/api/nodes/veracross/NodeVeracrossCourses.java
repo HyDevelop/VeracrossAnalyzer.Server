a4035052c25b5fee626b84f5e4eea1aeb76a17cc
Hykilpikonna <admin@moecraft.cc>
2019-09-03 08:48:47 -0400

    [+] Create api nodes to send backup files

---
+++ src/main/java/org/hydev/veracross/analyzer/api/nodes/veracross/NodeVeracrossCourses.java
@@ -0,0 +1,32 @@
+package org.hydev.veracross.analyzer.api.nodes.veracross;
+
+import org.hydev.veracross.analyzer.api.ApiAccess;
+import org.hydev.veracross.analyzer.api.ApiNode;
+
+import static org.hydev.veracross.analyzer.utils.ResourceReader.read;
+
+/**
+ * This api node obtains the courses information from Veracross and
+ * returns it to the requester.
+ * <p>
+ * Class created by the HyDEV Team on 2019-08-19!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-08-19 15:15
+ */
+public class NodeVeracrossCourses implements ApiNode
+{
+    @Override
+    public String path()
+    {
+        return "/api/veracross/courses";
+    }
+
+    @Override
+    public String process(ApiAccess access)
+    {
+        return read("/2019-05-30-courses-backup.json");
+    }
+}
