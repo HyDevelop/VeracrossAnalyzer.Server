751a4d7df9bae98a713c4af57dea6b93acc92af4
Hykilpikonna <admin@moecraft.cc>
2019-09-07 12:25:01 -0400

    [O] Optimize nodeAssignments implementation

--- src/main/java/org/hydev/veracross/analyzer/api/nodes/veracross/NodeAssignments.java
+++ src/main/java/org/hydev/veracross/analyzer/api/nodes/veracross/NodeAssignments.java
@@ -3,14 +3,10 @@ package org.hydev.veracross.analyzer.api.nodes.veracross;
 import com.google.gson.JsonObject;
 import org.hydev.veracross.analyzer.api.ApiAccess;
 import org.hydev.veracross.analyzer.api.JsonApiNode;
-import org.hydev.veracross.analyzer.api.JsonApiNode.GeneralReturnData;
 import org.hydev.veracross.analyzer.utils.CookieUtils;
 import org.hydev.veracross.sdk.VeracrossHttpClient;
-import org.hydev.veracross.sdk.model.VeracrossAssignments;
 
 import java.io.IOException;
-
-import static org.hydev.veracross.analyzer.VAConstants.GSON;
 
 /**
  * This api node obtains the assignments information from Veracross and
@@ -23,7 +19,7 @@ import static org.hydev.veracross.analyzer.VAConstants.GSON;
  * @author Vanilla (https://github.com/VergeDX)
  * @since 2019-08-16 15:09
  */
-public class NodeAssignments extends JsonApiNode<GeneralReturnData>
+public class NodeAssignments extends JsonApiNode
 {
     @Override
     public String path()
@@ -32,7 +28,7 @@ public class NodeAssignments extends JsonApiNode<GeneralReturnData>
     }
 
     @Override
-    protected GeneralReturnData processJson(ApiAccess access, JsonObject data) throws IOException
+    protected Object processJson(ApiAccess access, JsonObject data) throws IOException
     {
         // Create http client
         VeracrossHttpClient veracross = new VeracrossHttpClient();
@@ -41,7 +37,6 @@ public class NodeAssignments extends JsonApiNode<GeneralReturnData>
         CookieUtils.unwrap(veracross, data.get("token").getAsString());
 
         // Get it
-        VeracrossAssignments assignments = veracross.getAssignments(data.get("id").getAsLong());
-        return new GeneralReturnData(true, GSON.toJson(assignments));
+        return veracross.getAssignments(data.get("id").getAsLong());
     }
 }
