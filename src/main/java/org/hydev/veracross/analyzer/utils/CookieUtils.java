6e228abfa8ae6389e1f143c0c0f383b39acbe728
Hykilpikonna <admin@moecraft.cc>
2019-09-07 11:28:03 -0400

    [+] Create CookieUtils class

---
+++ src/main/java/org/hydev/veracross/analyzer/utils/CookieUtils.java
@@ -0,0 +1,16 @@
+package org.hydev.veracross.analyzer.utils;
+
+/**
+ * This class is an utility class for cookies.
+ * <p>
+ * Class created by the HyDEV Team on 2019-09-07!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-09-07 11:27
+ */
+public class CookieUtils
+{
+
+}
