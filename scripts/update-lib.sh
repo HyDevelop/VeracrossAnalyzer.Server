5e7df83135f621cafe076cec73c46d62d4213618
Hykilpikonna <admin@moecraft.cc>
2019-09-18 23:26:39 -0400

    [O] Remove the old libraries before updating them.

--- scripts/update-lib.sh
+++ scripts/update-lib.sh
@@ -1,4 +1,5 @@
 
 # Upload libraries
 ssh root@hydev.org "rm -rf /app/depl/veracross-analyzer/lib/*"
+rm /app/depl/veracross-analyzer/lib/*
 scp -r ../target/lib root@hydev.org:/app/depl/veracross-analyzer/
