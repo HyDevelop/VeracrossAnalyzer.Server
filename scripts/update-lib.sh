767878c015cf08b2e9750f386acfaa92daa41266
Hykilpikonna <admin@moecraft.cc>
2019-09-23 19:23:42 -0400

    [-] Remove unnecessary script

--- scripts/update-lib.sh
+++ scripts/update-lib.sh
@@ -1,5 +1,4 @@
 
 # Upload libraries
 ssh root@hydev.org "rm -rf /app/depl/veracross-analyzer/lib/*"
-rm /app/depl/veracross-analyzer/lib/*
 scp -r ../target/lib root@hydev.org:/app/depl/veracross-analyzer/
