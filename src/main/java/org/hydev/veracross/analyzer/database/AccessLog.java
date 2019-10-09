219fadae0ec1dc6f71e6c94aaff9e98728dae109
Hykilpikonna <admin@moecraft.cc>
2019-10-09 17:05:44 -0400

    [+] Create access log and courses pojo

---
+++ src/main/java/org/hydev/veracross/analyzer/database/AccessLog.java
@@ -0,0 +1,21 @@
+package org.hydev.veracross.analyzer.database;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * TODO: Write a description for this class!
+ * <p>
+ * Class created by the HyDEV Team on 2019-10-09!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-10-09 16:28
+ */
+@Entity
+@Table(name = "va_access_log")
+public class AccessLog
+{
+
+}
---
+++ src/main/java/org/hydev/veracross/analyzer/database/Courses.java
@@ -0,0 +1,20 @@
+package org.hydev.veracross.analyzer.database;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * TODO: Write a description for this class!
+ * <p>
+ * Class created by the HyDEV Team on 2019-10-09!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-10-09 16:56
+ */
+@Entity
+@Table(name = "va_courses")
+public class Courses
+{
+}
