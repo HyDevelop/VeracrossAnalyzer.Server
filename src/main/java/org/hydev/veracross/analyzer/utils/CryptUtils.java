671976aba8432d16d7ff4191f030a2c3d89d4f4f
Hykilpikonna <admin@moecraft.cc>
2019-10-09 17:06:02 -0400

    [+] Import cryptUtils

---
+++ src/main/java/org/hydev/veracross/analyzer/utils/CryptUtils.java
@@ -0,0 +1,119 @@
+package org.hydev.veracross.analyzer.utils;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.hydev.veracross.analyzer.utils.Base64Utils.decodeBase64C;
+import static org.hydev.veracross.analyzer.utils.Base64Utils.encodeBase64C;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/12/22 创建!
+ * Created by Hykilpikonna on 2018/12/22!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@SuppressWarnings("WeakerAccess")
+public class CryptUtils
+{
+    /**
+     * 加密一段字符串
+     *
+     * @param text 明文
+     * @param secret 密钥
+     * @return 密文
+     */
+    public static String encrypt(String text, Key secret)
+    {
+        try
+        {
+            return encryptHelper(text, secret);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 解密一段字符串
+     *
+     * @param text 密文
+     * @param secret 密钥
+     * @return 明文
+     */
+    public static String decrypt(String text, Key secret)
+    {
+        try
+        {
+            return decryptHelper(text, secret);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 加密一段明文字符串
+     *
+     * @param text 明文
+     * @param secret 密钥
+     * @return 密文
+     * @throws Exception 加密相关的异常
+     */
+    private static String encryptHelper(String text, Key secret) throws Exception
+    {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secret);
+        return encodeBase64C(cipher.doFinal(text.getBytes(UTF_8)));
+    }
+
+    /**
+     * 解密一段密文字符串
+     *
+     * @param text 密文
+     * @param secret 密钥
+     * @return 明文
+     * @throws Exception 加密相关的异常
+     */
+    private static String decryptHelper(String text, Key secret) throws Exception
+    {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secret);
+        return new String(cipher.doFinal(decodeBase64C(text)), UTF_8);
+    }
+
+    /**
+     * 用密码生成Key
+     *
+     * @param keySeed 密码
+     * @return Key
+     */
+    public static Key getKey(String keySeed)
+    {
+        if (keySeed == null) keySeed = System.getenv("AES_SYS_KEY");
+        if (keySeed == null) keySeed = System.getProperty("AES_SYS_KEY");
+        if (keySeed == null || keySeed.trim().length() == 0) throw new RuntimeException("KeySeed is null");
+
+        try
+        {
+            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+            secureRandom.setSeed(keySeed.getBytes());
+            KeyGenerator generator = KeyGenerator.getInstance("AES");
+            generator.init(secureRandom);
+            return generator.generateKey();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
