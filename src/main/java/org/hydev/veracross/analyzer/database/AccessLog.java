37e7b041725bb9f44a9276603e9cbe0af446a3a3
Hykilpikonna <admin@moecraft.cc>
2019-10-09 21:14:35 -0400

    [+] Create access log model

--- src/main/java/org/hydev/veracross/analyzer/database/AccessLog.java
+++ src/main/java/org/hydev/veracross/analyzer/database/AccessLog.java
@@ -1,7 +1,8 @@
 package org.hydev.veracross.analyzer.database;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import lombok.*;
+
+import javax.persistence.*;
 
 /**
  * TODO: Write a description for this class!
@@ -13,9 +14,29 @@ import javax.persistence.Table;
  * @author Vanilla (https://github.com/VergeDX)
  * @since 2019-10-09 16:28
  */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
 @Entity
 @Table(name = "va_access_log")
 public class AccessLog
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
 
+    @NonNull
+    @Column(name = "user")
+    private String user;
+
+    @NonNull
+    @Column(name = "action")
+    private String action;
+
+    @NonNull
+    @Column(name = "details")
+    private String details;
 }
