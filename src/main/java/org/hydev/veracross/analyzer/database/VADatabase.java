94dce674f2bcbaaef6a4760bad4717866ba01636
Hykilpikonna <admin@moecraft.cc>
2019-10-09 21:22:45 -0400

    [+] Create database class

---
+++ src/main/java/org/hydev/veracross/analyzer/database/VADatabase.java
@@ -0,0 +1,19 @@
+package org.hydev.veracross.analyzer.database;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ * This class is for database operations
+ * <p>
+ * Class created by the HyDEV Team on 2019-10-09!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-10-09 21:19
+ */
+public class VADatabase
+{
+
+}
