11e6cf3824b1eb5dae82c499c4f5919440a77134
Hykilpikonna <admin@moecraft.cc>
2019-10-12 14:12:24 -0400

    [+] Create json api config class

---
+++ src/main/java/org/hydev/veracross/analyzer/api/JsonApiConfig.java
@@ -0,0 +1,33 @@
+package org.hydev.veracross.analyzer.api;
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * Implementations of this class contains configuration info for json
+ * based api nodes.
+ * <p>
+ * Class created by the HyDEV Team on 2019-10-12!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-10-12 14:09
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JsonApiConfig
+{
+    /** Check for body length */
+    private int maxBodyLength = 2000;
+
+    /** Check for must-contain keys */
+    private ArrayList<String> mustContainKeys;
+
+    /** Check for value length */
+    private Map<String, Integer> lengthCheck;
+}
