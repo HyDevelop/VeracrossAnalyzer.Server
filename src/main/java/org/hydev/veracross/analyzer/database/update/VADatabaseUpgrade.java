0183d4ea668d95eeaa82812b1b3b4e98072c0471
Hykilpikonna <admin@moecraft.cc>
2019-11-17 18:17:43 -0500

    [+] Create database upgrade class

---
+++ src/main/java/org/hydev/veracross/analyzer/database/update/VADatabaseUpgrade.java
@@ -0,0 +1,15 @@
+package org.hydev.veracross.analyzer.database.update;
+
+/**
+ * This class is a utility class to upgrade the database.
+ * <p>
+ * Class created by the HyDEV Team on 2019-11-17!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-11-17 18:02
+ */
+public class VADatabaseUpgrade
+{
+}
