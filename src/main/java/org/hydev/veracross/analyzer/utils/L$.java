3d2cf8a69beb6dd10d45919b1d589f359643c78b
Hykilpikonna <admin@moecraft.cc>
2019-11-17 11:26:39 -0500

    [+] Create List$weetener class

---
+++ src/main/java/org/hydev/veracross/analyzer/utils/L$.java
@@ -0,0 +1,23 @@
+package org.hydev.veracross.analyzer.utils;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * List $weetener
+ * A fake array syntax sugar class. It's 2019 already why doesn't java
+ * have array.find(), array.map(), array.filter(), etc...
+ * <p>
+ * Class created by the HyDEV Team on 2019-11-14!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-11-14 22:29
+ */
+public class L$<T>
+{
+}
