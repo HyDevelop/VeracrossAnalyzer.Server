0b8717f22be36afffdfa454f4848093df480ffd7
Hykilpikonna <admin@moecraft.cc>
2019-11-17 17:57:38 -0500

    [+] Get user and return

--- src/main/java/org/hydev/veracross/analyzer/api/nodes/veracross/NodeLoginToken.java
+++ src/main/java/org/hydev/veracross/analyzer/api/nodes/veracross/NodeLoginToken.java
@@ -1,12 +1,16 @@
 package org.hydev.veracross.analyzer.api.nodes.veracross;
 
+import lombok.AllArgsConstructor;
 import org.hydev.veracross.analyzer.api.ApiAccess;
 import org.hydev.veracross.analyzer.api.JsonApiConfig;
 import org.hydev.veracross.analyzer.api.JsonApiNode;
+import org.hydev.veracross.analyzer.database.VADatabase;
 import org.hydev.veracross.analyzer.database.model.User;
 import org.hydev.veracross.analyzer.utils.CookieUtils;
 import org.hydev.veracross.analyzer.utils.CookieUtils.CookieData;
 import org.hydev.veracross.sdk.VeracrossHttpClient;
+
+import java.util.Date;
 
 import static org.hydev.veracross.analyzer.VAConstants.LENGTH_TOKEN;
 import static org.hydev.veracross.analyzer.utils.CookieUtils.unwrap;
@@ -39,6 +43,11 @@ public class NodeLoginToken extends JsonApiNode<NodeLoginToken.Model>
         // Update token TODO: Check if actually online
         String csrf = veracross.getCsrfToken();
         String newToken = CookieUtils.wrap(cookie.getUsername(), veracross, csrf);
+
+        // Get user from database
+        User user = VADatabase.getUser(cookie.getUsername(), veracross);
+
+        return new ReturnModel(user);
     }
 
     @Override
