65822a04aad5f8c61a79bcc5068e6cb00f73feff
Hykilpikonna <admin@moecraft.cc>
2019-11-17 19:36:15 -0500

    [T] Add L$ tests

--- src/test/java/L$Test.java
+++ src/test/java/L$Test.java
@@ -21,8 +21,18 @@ public class L$Test
         // JS: let list = [{name: 'User1', age: 14}, {name: 'User2', age: 15}, {name: 'MyCat', age: 99999}]
         L$<User> list = l$(new User("User1", 14), new User("User2", 15), new User("MyCat", 99999));
 
-        // JS: let age = list.find(user => user.name == 'User2').age
-        int age = list.find(user -> user.name.equals("User2")).age;
+        // JS: let find = list.find(user => user.name == 'User2')
+        User find = list.find(user -> user.name.equals("User2"));
+
+        // JS: list.forEach((value, i) => console.log(`${value} - ${i}`))
+        list.forEach((value, i) -> System.out.println(value + " - " + i));
+
+        // JS: list.forEach((value, i, self) => console.log(`${value} - ${i} of ${self}`))
+        list.forEach((value, i, self) -> System.out.println(value + " - " + i + " of " + self));
+
+        // 比 JS 更甜的语法糖w
+        // JS: let last = list[list.length - 1]
+        User last = list.last();
     }
 
     @AllArgsConstructor
