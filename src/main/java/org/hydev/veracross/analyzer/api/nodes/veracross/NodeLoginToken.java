e03a71ef8011f9b19901233a13d612485a5302f8
Hykilpikonna <admin@moecraft.cc>
2019-11-29 20:20:22 -0500

    [U] Update usage in NodeLoginToken

--- src/main/java/org/hydev/veracross/analyzer/api/nodes/veracross/NodeLoginToken.java
+++ src/main/java/org/hydev/veracross/analyzer/api/nodes/veracross/NodeLoginToken.java
@@ -6,14 +6,12 @@ import org.hydev.veracross.analyzer.api.JsonApiConfig;
 import org.hydev.veracross.analyzer.api.JsonApiNode;
 import org.hydev.veracross.analyzer.database.VADatabase;
 import org.hydev.veracross.analyzer.database.model.User;
-import org.hydev.veracross.analyzer.utils.CookieUtils;
-import org.hydev.veracross.analyzer.utils.CookieUtils.CookieData;
+import org.hydev.veracross.analyzer.utils.CookieData;
 import org.hydev.veracross.sdk.VeracrossHttpClient;
 
 import java.util.Date;
 
 import static org.hydev.veracross.analyzer.VAConstants.LENGTH_TOKEN;
-import static org.hydev.veracross.analyzer.utils.CookieUtils.unwrap;
 
 /**
  * Login with token
@@ -38,11 +36,10 @@ public class NodeLoginToken extends JsonApiNode<NodeLoginToken.Model>
     {
         // Create http client with token
         VeracrossHttpClient veracross = new VeracrossHttpClient();
-        CookieData cookie = unwrap(veracross, data.token);
+        CookieData cookie = new CookieData(data.token).store(veracross);
 
         // Update token TODO: Check if actually online
-        String csrf = veracross.getCsrfToken();
-        String newToken = CookieUtils.wrap(cookie.getUsername(), veracross, csrf);
+        cookie.setCsrf(veracross.getCsrfToken());
 
         // Get user from database
         User user = User.get(cookie.getUsername());
@@ -54,7 +51,7 @@ public class NodeLoginToken extends JsonApiNode<NodeLoginToken.Model>
         user.setLastLogin(new Date());
 
         // Update token
-        user.setToken(CookieUtils.wrap(cookie));
+        user.setToken(cookie.wrap());
 
         // Save user
         VADatabase.saveOrUpdate(user);
