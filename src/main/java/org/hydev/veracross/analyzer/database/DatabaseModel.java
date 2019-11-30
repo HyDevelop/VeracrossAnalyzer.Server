d6a72370a599a0d408d2271d5a6adb72c05b8933
Hykilpikonna <admin@moecraft.cc>
2019-11-29 20:53:51 -0500

    [+] Create DatabaseModel class

---
+++ src/main/java/org/hydev/veracross/analyzer/database/DatabaseModel.java
@@ -0,0 +1,15 @@
+package org.hydev.veracross.analyzer.database;
+
+/**
+ * TODO: Write a description for this class!
+ * <p>
+ * Class created by the HyDEV Team on 2019-11-29!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-11-29 20:48
+ */
+public abstract class DatabaseModel<T>
+{
+}
