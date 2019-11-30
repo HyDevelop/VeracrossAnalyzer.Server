7e8ec5ecfce441afbd01b067b9a594dc92fae7d6
Hykilpikonna <admin@moecraft.cc>
2019-11-29 21:39:45 -0500

    [O] Separate login with processing after login

--- src/main/java/org/hydev/veracross/analyzer/api/nodes/veracross/NodeLoginToken.java
+++ src/main/java/org/hydev/veracross/analyzer/api/nodes/veracross/NodeLoginToken.java
@@ -4,11 +4,11 @@ import lombok.AllArgsConstructor;
 import org.hydev.veracross.analyzer.api.ApiAccess;
 import org.hydev.veracross.analyzer.api.JsonApiConfig;
 import org.hydev.veracross.analyzer.api.JsonApiNode;
-import org.hydev.veracross.analyzer.database.VADatabase;
 import org.hydev.veracross.analyzer.database.model.User;
 import org.hydev.veracross.analyzer.utils.CookieData;
 import org.hydev.veracross.sdk.VeracrossHttpClient;
 
+import java.io.IOException;
 import java.util.Date;
 
 import static org.hydev.veracross.analyzer.VAConstants.LENGTH_TOKEN;
@@ -35,27 +35,35 @@ public class NodeLoginToken extends JsonApiNode<NodeLoginToken.Model>
     protected Object processJson(ApiAccess access, Model data) throws Exception
     {
         // Create http client with token
-        VeracrossHttpClient veracross = new VeracrossHttpClient();
-        CookieData cookie = new CookieData(data.token).store(veracross);
+        VeracrossHttpClient client = new VeracrossHttpClient();
+        CookieData cookie = new CookieData(data.token).store(client);
 
         // Update token TODO: Check if actually online
-        cookie.setCsrf(veracross.getCsrfToken());
+        cookie.setCsrf(client.getCsrfToken());
 
+        // After login
+        return afterLogin(client, cookie);
+    }
+
+    /**
+     * Process after login
+     *
+     * @param client Veracross http client
+     * @param cookie Cookie data
+     * @return ReturnModel data
+     */
+    public static ReturnModel afterLogin(VeracrossHttpClient client, CookieData cookie) throws IOException
+    {
         // Get user from database
         User user = User.get(cookie.getUsername());
 
         // If user doesn't exist, register.
-        if (user == null) user = User.register(cookie.getUsername(), veracross);
+        if (user == null) user = User.register(cookie.getUsername(), client);
 
-        // Update last login
-        user.setLastLogin(new Date());
+        // Update last login and token, and then save
+        user.lastLogin(new Date()).token(cookie.wrap()).save();
 
-        // Update token
-        user.setToken(cookie.wrap());
-
-        // Save user
-        VADatabase.saveOrUpdate(user);
-
+        // Return
         return new ReturnModel(user, false);
     }
 
