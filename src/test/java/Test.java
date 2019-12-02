131755022cdd8311a35936207bbfc2731e8f86a4
Hykilpikonna <admin@moecraft.cc>
2019-12-02 16:27:23 -0500

    [-] Remove tests

---
+++ src/test/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * TODO: Write a description for this class!
+ * <p>
+ * Class created by the HyDEV Team on 2019-12-02!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-12-02 16:26
+ */
+public class Test
+{
+}
