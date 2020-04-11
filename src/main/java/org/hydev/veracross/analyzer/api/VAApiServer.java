97f1e5d32930d15a985d8b800b953912eaddf9c7
Hykilpikonna <admin@moecraft.cc>
2020-04-11 16:58:06 -0400

    [-] Remove deprecated grading api

--- src/main/java/org/hydev/veracross/analyzer/api/VAApiServer.java
+++ src/main/java/org/hydev/veracross/analyzer/api/VAApiServer.java
@@ -59,7 +59,6 @@ public class VAApiServer
 
                 new NodeAssignments(),
                 new NodeCourses(),
-                new NodeGrading(),
                 new NodeGradingTerm(),
                 new NodeMarkAsRead()
         );
