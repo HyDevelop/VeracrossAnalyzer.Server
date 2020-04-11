036ae36d4c238570a06d9d8cd0e79a5b1a44bb9d
Hykilpikonna <admin@moecraft.cc>
2020-04-11 18:08:06 -0400

    [+] Create NodeAllCourses

---
+++ src/main/java/org/hydev/veracross/analyzer/api/nodes/veracross/NodeAllCourses.java
@@ -0,0 +1,56 @@
+package org.hydev.veracross.analyzer.api.nodes.veracross;
+
+import org.hydev.veracross.analyzer.api.ApiAccess;
+import org.hydev.veracross.analyzer.api.JsonApiConfig;
+import org.hydev.veracross.analyzer.api.JsonApiNode;
+import org.hydev.veracross.analyzer.api.JsonKnownError;
+import org.hydev.veracross.analyzer.database.model.Course;
+import org.hydev.veracross.analyzer.utils.CookieData;
+import org.hydev.veracross.sdk.VeracrossHttpClient;
+
+import static org.hydev.veracross.analyzer.VAConstants.LENGTH_TOKEN;
+
+/**
+ * Returns all courses for course selection
+ * <p>
+ * Class created by the HyDEV Team on 2020-04-11!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2020-04-11 17:44
+ */
+public class NodeAllCourses extends JsonApiNode<NodeAllCourses.Model>
+{
+    @Override
+    public String path()
+    {
+        return "/api/directory";
+    }
+
+    @Override
+    protected JsonApiConfig<Model> config()
+    {
+        return new JsonApiConfig<>(Model.class).key("token", LENGTH_TOKEN);
+    }
+
+    protected static class Model
+    {
+        String token;
+    }
+
+    @Override
+    protected Object processJson(ApiAccess access, Model data) throws Exception
+    {
+        // Create http client
+        VeracrossHttpClient veracross = new VeracrossHttpClient();
+
+        // Unwrap cookies
+        CookieData cookie = new CookieData(data.token).store(veracross);
+
+        // Validate login
+        if (!veracross.validateLogin()) throw new JsonKnownError("Login expired!");
+
+        return Course.getAll();
+    }
+}
