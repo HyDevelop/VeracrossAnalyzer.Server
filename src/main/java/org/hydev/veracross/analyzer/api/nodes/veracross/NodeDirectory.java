011750b3561c842d82f4391f06ec3850fac82623
Hykilpikonna <admin@moecraft.cc>
2020-04-11 18:07:32 -0400

    [O] Optimize directory return type

--- src/main/java/org/hydev/veracross/analyzer/api/nodes/veracross/NodeDirectory.java
+++ src/main/java/org/hydev/veracross/analyzer/api/nodes/veracross/NodeDirectory.java
@@ -1,15 +1,16 @@
 package org.hydev.veracross.analyzer.api.nodes.veracross;
 
-import com.google.gson.reflect.TypeToken;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.hydev.veracross.analyzer.api.ApiAccess;
 import org.hydev.veracross.analyzer.api.JsonApiConfig;
 import org.hydev.veracross.analyzer.api.JsonApiNode;
 import org.hydev.veracross.analyzer.utils.CookieData;
 import org.hydev.veracross.sdk.VeracrossHttpClient;
 
-import java.util.ArrayList;
+import java.util.List;
 
-import static org.hydev.veracross.analyzer.VAConstants.GSON;
+import static java.util.stream.Collectors.toList;
 import static org.hydev.veracross.analyzer.VAConstants.LENGTH_TOKEN;
 
 /**
@@ -44,14 +45,15 @@ public class NodeDirectory extends JsonApiNode<NodeDirectory.Model>
     /**
      * Return model, used to filter irrelevant data
      */
+    @Data @AllArgsConstructor
     protected static class ReturnModel
     {
-        int current_grade_id;
-        String all_classes;
+        int grade;
+        String classes;
     }
 
     // TODO: This method of caching only works for the one school, have to be modified if other schools are added.
-    private static String directoryJsonCache;
+    private static List<ReturnModel> directoryJsonCache;
     private static long cacheTime = 0;
 
     @Override
@@ -67,8 +69,8 @@ public class NodeDirectory extends JsonApiNode<NodeDirectory.Model>
         if (System.currentTimeMillis() - cacheTime > 24 * 3600000)
         {
             // Update cache
-            directoryJsonCache = GSON.toJson(GSON.fromJson(GSON.toJson(veracross.getDirectoryStudents()),
-                new TypeToken<ArrayList<ReturnModel>>(){}.getType()));
+            directoryJsonCache = veracross.getDirectoryStudents().stream()
+                .map(s -> new ReturnModel(s.getCurrentGradeId(), s.getAllClasses())).collect(toList());
             cacheTime = System.currentTimeMillis();
         }
 
