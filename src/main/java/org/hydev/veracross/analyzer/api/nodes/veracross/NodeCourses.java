d660f63a35a0a0d25ebe1f609c7ff1d42089098e
Hykilpikonna <admin@moecraft.cc>
2020-04-26 13:50:38 -0400

    [O] Trim after replace

--- src/main/java/org/hydev/veracross/analyzer/api/nodes/veracross/NodeCourses.java
+++ src/main/java/org/hydev/veracross/analyzer/api/nodes/veracross/NodeCourses.java
@@ -113,7 +113,7 @@ public class NodeCourses extends JsonApiNode<NodeCourses.Model>
      */
     public static String detectLevel(String name)
     {
-        name = name.trim().replace("\\([^)]*\\)", "");
+        name = name.replaceAll("\\([^()]*\\)", "").trim();
 
         // Common ones
         if (name.startsWith("AP")) return AP;
